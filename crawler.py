cbd3cc7ded28b1a16aec6fc6910831cb8b418be3
happy <middlet@gmail.com>
2013-05-11 10:21:41 +0100

    added simple logging to the crawler

--- crawler.py
+++ crawler.py
@@ -10,23 +10,45 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+import logging
+import logging.handlers
+
+
+class MyRotatingHandler(logging.handlers.RotatingFileHandler):
+    """
+    A modified form of the stadard rotating logger
+    this creates a new directory for each day. otherwise it
+    will just rotate on size of file
+    """
+    pass
 
 class StdOutListener(StreamListener):
-  """ 
-  A listener handles tweets are the received from the stream. 
-  This is a basic listener that just prints received tweets to stdout.
-  """
-  def on_data(self, data):
-    print data
-    return True
+    """ 
+    A listener handles tweets are the received from the stream. 
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self, data):
+        #print data
+        my_logger.info(data)
+        return True
   
-  def on_error(self, status):
-    print status
+    def on_error(self, status):
+        my_logger.error(status)
+        #print status
 
 if __name__ == '__main__':
-  l = StdOutListener()
-  auth = OAuthHandler(consumer_key, consumer_secret)
-  auth.set_access_token(access_token, access_token_secret)
+    LOG_FNAME = 'log/rotate.log'
+    # Set up a specific logger with our desired output level
+    my_logger = logging.getLogger('MyLogger')
+    my_logger.setLevel(logging.DEBUG)
 
-  stream = Stream(auth, l)	
-  stream.filter(track=['evacuate'])
+    # Add the log message handler to the logger
+    handler = MyRotatingHandler(LOG_FNAME, maxBytes=65536, backupCount=10)
+    my_logger.addHandler(handler)
+    
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)	
+    stream.filter(track=['facup'])
